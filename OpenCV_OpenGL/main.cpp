1718f8562e2a686cdacf4c1bd70dfe79abc79693
Quincy <q.baljeu@gmail.com>
2020-05-08 13:45:58 +0200

    [FIX] Cleaned up comments and includes

--- OpenCV_OpenGL/main.cpp
+++ OpenCV_OpenGL/main.cpp
@@ -1,23 +1,20 @@
 // ************************************************************************************************************************************************
-// ********************************************** Camera ******************************************************************************************
+// ********************************************** BurgerBros ******************************************************************************************
 // ************************************************************************************************************************************************
 
-// Dit programma leest continue een camera beeld in en toont deze op het scherm
-// Bij sommige camera's is het beeld gespiegeld. Dit kan worden opgelost 
-// met de flip-functie van OpenCV.
-// 
-// Jan Oostindie, dd 22-2-2015
-
+//OpenGl includes
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "tigl.h"
 #include <glm/gtc/matrix_transform.hpp>
 using tigl::Vertex;
 
+
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "opengl32.lib")
 
+//OpenCV includes
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgproc/imgproc.hpp" 
 #include "opencv2/highgui/highgui.hpp"
@@ -31,5 +28,5 @@ GLFWwindow* window;
 
 int main(void)
 {
-    return 0;
+	return 1;
 }
