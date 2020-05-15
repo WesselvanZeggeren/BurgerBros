8ee58c32467214cf3400d70258b9b1483ec9be9e
Wessel <wesselvanz@gmail.com>
2020-05-15 14:30:19 +0200

    Made a script that finds colors
    (without changes to openCV settings or libs)

--- OpenCV_OpenGL/Camera.cpp
+++ OpenCV_OpenGL/Camera.cpp
@@ -6,23 +6,33 @@
 #include <string>
 #include "Camera.h"
 	
-VideoCapture cap;
-
-Mat frame, src_gray; 
+VideoCapture cap; 
 
 int maxThresh = 255;
+char filename[100];
 
 RNG rng(12345);
 
-char filename[100];
+Mat frame;
+Mat hsv_frame;
+Mat gray_frame;
+
+void distance(Mat& into);
+
+void open(int amount, Mat& into);
+void close(int amount, Mat& into);
+
+void erode(int amount, Mat& into);
+void dilate(int amount, Mat& into);
 
 int Camera::SetUpCamera(int cameraNumber)
 {
-	cap.open(cameraNumber);
 
+	cap.open(cameraNumber);
 
 	if (!cap.isOpened())
 	{
+	
 		cout << "Cannot open the video cam" << endl;
 		return -1;
 	}
@@ -31,38 +41,61 @@ int Camera::SetUpCamera(int cameraNumber)
 	double height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	cout << "Frame size : " << width << " x " << height << endl;
 
-	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
+	namedWindow("Real", CV_WINDOW_AUTOSIZE);
+	namedWindow("Altered", CV_WINDOW_AUTOSIZE);
 
 	while (1)
 	{
+
+		Mat mask1, mask2, mask3;
+		Mat color_only_frame;
 
 		bool bSuccess = cap.read(frame);
 		flip(frame, frame, 3);
 
 		if (!bSuccess)
 		{
+		
 			cout << "Cannot read a frame from video stream" << endl;
 			break;
 		}
 
-		imshow("MyVideo", frame);
+		imshow("Real", frame);
+
+		cvtColor(frame, hsv_frame, COLOR_BGR2HLS);
+
+		inRange(hsv_frame, Scalar(35, 50, 20), Scalar(50, 255, 255), mask1);
+		inRange(hsv_frame, Scalar(35, 50, 20), Scalar(50, 255, 255), mask2);
+
+		bitwise_or(mask1, mask2, mask3);
+
+		open(1, mask3);
+		close(1, mask3);
+
+		bitwise_and(frame, frame, color_only_frame, mask3);
+
+		imshow("Altered", color_only_frame);
 
 		SnapShot();
 
 		if (waitKey(1) == 27)
 		{
+			
 			cout << "esc key is pressed by user" << endl;
 			break;
 		}
 	}
+
 	return 0;
 }
 
 void Camera::SnapShot()
 {
+
 	char key = cv::waitKey(30);
 
-	if (key == 'q') {
+	if (key == 'q') 
+	{
 
 		sprintf_s(filename, "C:/Users/maart/OneDrive/Afbeeldingen/FrameCap.jpg"); // select your folder - filename is "Frame_n"
 		cv::waitKey(10);
@@ -76,20 +109,81 @@ void Camera::SnapShot()
 
 void Camera::GetContour(int threshold, void* )
 {
+
 	Mat contourOutput;
 	vector <vector<Point>> contours;
 	vector <Vec4i> hierarchy;
-	cvtColor(frame, src_gray, CV_BGR2GRAY);
-	Canny(src_gray, contourOutput, threshold, threshold* 2, 3);
+	
+	cvtColor(frame, gray_frame, CV_BGR2GRAY);
+	
+	Canny(gray_frame, contourOutput, threshold, threshold* 2, 3);
 	findContours(contourOutput, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 	Mat drawing = Mat::zeros(contourOutput.size(), CV_8UC3);
+	
 	for (int i = 0; i < contours.size(); i++)
 	{
+
 		Scalar color = Scalar(rng.uniform(255, 255), rng.uniform(255, 255), rng.uniform(255, 255));
 		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
 	}
 
 	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
 	imshow("Contours", drawing);
+}
+
+// voegt distance toe
+void distance(Mat& into)
+{
+
+	Mat holder;
+
+	distanceTransform(into, holder, DIST_L2, 3);
+	normalize(holder, into, 0, 1.0, NORM_MINMAX);
+}
+
+// erode's [amount] keer en delate's daarna [amount] keer
+void open(int amount, Mat& into)
+{
+
+	erode(amount, into);
+	dilate(amount, into);
+}
+
+// dilate's [amount] keer en erode's daarna [amount] keer
+void close(int amount, Mat& into)
+{
+
+	dilate(amount, into);
+	erode(amount, into);
+}
+
+// erode functie
+void erode(int amount, Mat& into)
+{
+
+	Mat holder;
+	Mat element = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, 1));
+
+	for (int i = 0; i < amount; i++)
+	{
+
+		erode(into, holder, element);
+		into = holder;
+	}
+}
+
+// delate functie
+void dilate(int amount, Mat& into)
+{
+
+	Mat holder;
+	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(5, 5), Point(-1, 1));
+
+	for (int i = 0; i < amount; i++)
+	{
+
+		dilate(into, holder, element);
+		into = holder;
+	}
 }
--- OpenCV_OpenGL/main.cpp
+++ OpenCV_OpenGL/main.cpp
@@ -10,25 +10,25 @@
 #include "Camera.h"
 using tigl::Vertex;
 
-
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "opengl32.lib")
 
-	//OpenCV includes
-	#include <opencv2/opencv.hpp>
-	#include "opencv2/imgproc/imgproc.hpp" 
-	#include "opencv2/highgui/highgui.hpp"
-	#include <iostream>
-	#include <string>
+//OpenCV includes
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp" 
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <string>
 
-	using namespace cv;
-	using namespace std;
+using namespace cv;
+using namespace std;
 
-	GLFWwindow* window;
+GLFWwindow* window;
 
 int main(void)
 {
+
 	Camera cam;
 	cam.SetUpCamera(0);
 	return 1;
