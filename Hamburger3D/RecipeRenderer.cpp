5422e7eed7252b1d414656d963f083a40b59c78d
Quincy <q.baljeu@gmail.com>
2020-05-18 14:52:22 +0200

    Added reciperenderer prototypes

---
+++ Hamburger3D/RecipeRenderer.cpp
@@ -0,0 +1,17 @@
+#include "RecipeRenderer.h"
+
+RecipeRenderer::RecipeRenderer() 
+{
+}
+
+RecipeRenderer::~RecipeRenderer()
+{
+}
+
+void RecipeRenderer::printRecipe(std::list<BurgerIngredient*> recipe) 
+{
+	for (BurgerIngredient* ingredient : recipe) 
+	{
+	}
+
+}
