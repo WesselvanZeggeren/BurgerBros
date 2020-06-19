ac3edbf17fc5137d0cdf5e2235c65d6084755114
Quincy <q.baljeu@gmail.com>
2020-06-19 14:39:08 +0200

    [ADD] added burger comparTo method

--- Hamburger3D/main.cpp
+++ Hamburger3D/main.cpp
@@ -9,7 +9,8 @@ Camera camera;
 
 int main(void)
 {
-	if (camera.SetUpCamera(0) == 0)
+	/*
+	if (0)
 	{
 		game.startGame(camera.height, camera.width, camera);
 		return 0;
@@ -17,7 +18,33 @@ int main(void)
 	}
 	else 
 	{
+		
 		return 0;
 	}
+	*/
+
+	Recipe recipe;
+	Burger burger1;
+	Burger burger2;
+	Burger burger3;
+
+	recipe.generateRecipe();
+
+	//2 of the same burger
+	burger1 = recipe.convertToBurger();
+	burger2 = recipe.convertToBurger();
+
+	recipe.generateRecipe();
+
+	//different burger
+	burger3 = recipe.convertToBurger();
+
+	//should return true
+	std::cout << burger1.compareTo(burger2) << "\n";
+
+	//should return false
+	std::cout << burger1.compareTo(burger3) << "\n";
+
+	
 
 }
