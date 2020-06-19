12757042d5d8a9a528a95da9a6eff51a6133b71e
Quincy <q.baljeu@gmail.com>
2020-06-19 14:43:16 +0200

    [FIX] removed test code in main

--- Hamburger3D/main.cpp
+++ Hamburger3D/main.cpp
@@ -9,8 +9,7 @@ Camera camera;
 
 int main(void)
 {
-	/*
-	if (0)
+	if (camera.SetUpCamera(0) == 0)
 	{
 		game.startGame(camera.height, camera.width, camera);
 		return 0;
@@ -18,33 +17,7 @@ int main(void)
 	}
 	else 
 	{
-		
 		return 0;
 	}
-	*/
-
-	Recipe recipe;
-	Burger burger1;
-	Burger burger2;
-	Burger burger3;
-
-	recipe.generateRecipe();
-
-	//2 of the same burger
-	burger1 = recipe.convertToBurger();
-	burger2 = recipe.convertToBurger();
-
-	recipe.generateRecipe();
-
-	//different burger
-	burger3 = recipe.convertToBurger();
-
-	//should return true
-	std::cout << burger1.compareTo(burger2) << "\n";
-
-	//should return false
-	std::cout << burger1.compareTo(burger3) << "\n";
-
-	
 
 }
